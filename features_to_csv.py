a7e8b0324c0added95cecb4896a444c5ed4ee532
056475 <youngp6475@g.comalisd.org>
2024-03-25 11:28:45 -0500

    Made feature extraction more precise

--- features_to_csv.py
+++ features_to_csv.py
@@ -10,8 +10,8 @@ norm_rock_dir = "./dataset/rock/"
 norm_hiphop_dir = "./dataset/hiphop/"
 norm_rock_files = glob(norm_rock_dir + "*.wav")
 norm_hiphop_files = glob(norm_hiphop_dir + "*.wav")
-rock_genre_list = ["rock" for i in range(len(norm_rock_files))]
-hiphop_genre_list = ["hiphop" for i in range(len(norm_rock_files))]
+#rock_genre_list = ["rock" for i in range(len(norm_rock_files))]
+#hiphop_genre_list = ["hiphop" for i in range(len(norm_rock_files))]
 
 # Define the functions to extract the features
 fn_list_i = [
@@ -48,23 +48,53 @@ def get_feature_vector(ts, sr):
     feature_vector = feat_vector_i + feat_vector_ii
     return feature_vector
 
+def split_audio_file(audio_file, sec_len):
+    """
+    Splits an audio file into segments of specified length
+    Args:
+        audio_file (str): String pathname of .wav audio file
+        sec_len (int): number of seconds long the audio segment should be
+
+    Returns:
+        A list of the audio sections and the sample rate of the audio in tuple format (audio_sections, sample_rate)
+    """
+    time_series, sample_rate = librosa.load(audio_file)
+    sec_samples = sec_len * sample_rate
+    total_sections = int(np.ceil(len(time_series)/sec_samples))
+    audio_sections = []
+    for i in range(total_sections):
+        start_sample = i * sec_samples
+        end_sample = min((i + 1) * sec_samples, len(time_series))
+        section = time_series[start_sample:end_sample]
+        audio_sections.append(section)
+    if audio_sections[-1].size < sample_rate:
+        audio_sections.pop(-1)
+    return audio_sections, sample_rate
+
 
 # Feature extraction
 norm_rock_feat = []
 norm_hiphop_feat = []
 for file in norm_rock_files:
-    ts, sr = librosa.load(file)
-    feature_vector = get_feature_vector(ts, sr)
-    norm_rock_feat.append(feature_vector)
+    #ts, sr = librosa.load(file)
+    #feature_vector = get_feature_vector(ts, sr)
+    #norm_rock_feat.append(feature_vector)
+    audio_sections, sr = split_audio_file(file, 3)
+    for ts in audio_sections:
+        feature_vector = get_feature_vector(ts, sr)
+        norm_rock_feat.append(feature_vector)
 for file in norm_hiphop_files:
-    ts, sr = librosa.load(file)
-    feature_vector = get_feature_vector(ts, sr)
-    norm_hiphop_feat.append(feature_vector)
+    #ts, sr = librosa.load(file)
+    #feature_vector = get_feature_vector(ts, sr)
+    #norm_hiphop_feat.append(feature_vector)
+    audio_sections, sr = split_audio_file(file, 3)
+    for ts in audio_sections:
+        feature_vector = get_feature_vector(ts, sr)
+        norm_hiphop_feat.append(feature_vector)
 
 # Define CSV file needed info
-outfile = "song_features.csv"
+outfile = "song_features_more.csv"
 headers = [
-    "file_name",
     "genre",
     "chroma_stft_mean",
     "chroma_stft_dev",
@@ -79,16 +109,18 @@ headers = [
 ]
 
 # Format arrays in way that CSV library likes
-norm_rock_files = list(map(os.path.basename, norm_rock_files))
+rock_genre_list = ["rock" for i in range(np.shape(np.array(norm_rock_feat))[0])]
+hiphop_genre_list = ["hiphop" for i in range(np.shape(np.array(norm_hiphop_feat))[0])]
+#norm_rock_files = list(map(os.path.basename, norm_rock_files))
 rock_genre_list = np.array(rock_genre_list)
-norm_rock_files = np.array(norm_rock_files)
-norm_hiphop_files = list(map(os.path.basename, norm_hiphop_files))
+#norm_rock_files = np.array(norm_rock_files)
+#norm_hiphop_files = list(map(os.path.basename, norm_hiphop_files))
 hiphop_genre_list = np.array(hiphop_genre_list)
-norm_hiphop_files = np.array(norm_hiphop_files)
+#norm_hiphop_files = np.array(norm_hiphop_files)
 norm_rock_feat = np.hstack((rock_genre_list.reshape(-1, 1), norm_rock_feat))
-norm_rock_feat = np.hstack((norm_rock_files.reshape(-1, 1), norm_rock_feat))
+#norm_rock_feat = np.hstack((norm_rock_files.reshape(-1, 1), norm_rock_feat))
 norm_hiphop_feat = np.hstack((hiphop_genre_list.reshape(-1, 1), norm_hiphop_feat))
-norm_hiphop_feat = np.hstack((norm_hiphop_files.reshape(-1, 1), norm_hiphop_feat))
+#norm_hiphop_feat = np.hstack((norm_hiphop_files.reshape(-1, 1), norm_hiphop_feat))
 
 # Export to CSV File
 with open(outfile, "w", newline="") as f:
