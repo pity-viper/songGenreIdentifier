72352d11cc39baacc7b97065d63ed1eba3eec418
095440 <horsll5440@g.comalisd.org>
2024-04-18 11:58:27 -0500

    User Input

---
+++ InputQuestionmark.py
@@ -0,0 +1,30 @@
+import os
+import time
+from tkinter import *
+import glob
+from tkinter.filedialog import askopenfilename
+from train_model import predict_song_genre
+'''
+def submit():
+    entry.delete(0,END)
+    entry.config(state="disabled")
+    print(predict_song_genre(file))
+    #time.sleep(5)
+    #entry.config(state="normal")
+    #entry.insert(0,"testing")
+'''
+file = askopenfilename()
+mfile = os.path.abspath("TEST1.pkl")
+print(predict_song_genre(file, mfile))
+
+window = Tk()
+entry = Entry()
+#submit = Button(window, text="Submit",command=submit)
+#submit.pack(side="bottom")
+entry.config(bg=("Black"))
+entry.config(fg=("Green"))
+entry.config(font=("Times",40))
+entry.pack()
+window.mainloop()
+print(os.path.abspath("features_to_csv.py"))
+
--- train_model.py
+++ train_model.py
@@ -12,48 +12,32 @@ import joblib
 from features_to_csv import get_feature_vectors, load_files
 
 
-def predict_song_genre(file_path, genre="predict"):
+# features from the csv file that we need to train the ai on
+# all the other data is just white noise
+features = ['chroma_stft_mean', 'chroma_stft_dev', 'spectral_centroid_mean', 'spectral_centroid_dev',
+            'spectral_rolloff_mean', 'spectral_rolloff_dev', 'mfcc_mean', 'mfcc_dev', 'rms_mean', 'rms_dev',
+            'zero_crossing_rate_mean', 'zero_crossing_rate_dev']
+
+
+def predict_song_genre(song_file_path, model_file_path, genre="predict", testing=False):
     """
     Predict the genre of a given song, or validate that the model correctly predicts the genre of a song
     Args:
-        file_path (str): Path to the song file on the system
+        song_file_path (str): Path to the song file on the system
+        model_file_path (str): Path to the model .pkl file on the system
         genre (str): Optional parameter for the genre you expect the audio file to be.
+        testing (bool): Optional parameter for if you are testing the functionality of the model
 
     Returns:
         tuple: (Song name and expected genre, (predicted genre, count of each genre prediction))
     """
-    test_data = []
-    test_data.extend(get_feature_vectors(file_path, audio_genre=genre))
-    test = pd.DataFrame.from_records(test_data, columns=headers)
-    X_test = test.loc[:, features]
 
-    prediction = clf.predict(X_test)
-    counts = pd.DataFrame(prediction).value_counts()
-    prediction_mode = pd.DataFrame(prediction).mode()
-    return f"({genre}) {os.path.basename(path)}", (num_to_genre[prediction_mode.iloc[0][0]], counts)
-
-
-if __name__ == "__main__":
-    # Figure out what to import the csv file
-    df = pd.read_csv('song_features_4genre_v2.csv', index_col='file_name')
-
-    # features from the csv file that we need to train the ai on
-    # all the other data is just white noise
-    features = ['chroma_stft_mean', 'chroma_stft_dev', 'spectral_centroid_mean', 'spectral_centroid_dev',
-                'spectral_rolloff_mean', 'spectral_rolloff_dev', 'mfcc_mean', 'mfcc_dev', 'rms_mean', 'rms_dev',
-                'zero_crossing_rate_mean', 'zero_crossing_rate_dev']
-
-    # the genre in the dataset is a string and the ai cant run on a string, so you change it to a number here
-    df['genre'] = df.genre.map({'rock': 0, 'hiphop': 1, 'pop': 2, 'country': 3})
-
-    # create x and y
-    # x is a separate file with just the train data
-    # y is a separate file with the corresponding target values
-    X = df.loc[:, features]
-    y = df['genre']
-
-    # split the dataset into testing and training data using train_test_split
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+    num_to_genre = {
+        0: 'rock',
+        1: 'hiphop',
+        2: 'pop',
+        3: 'country'
+    }
 
     headers = [
         "file_name",
@@ -72,6 +56,37 @@ if __name__ == "__main__":
         "zero_crossing_rate_dev"
     ]
 
+    test_data = []
+    model = joblib.load(model_file_path)
+    test_data.extend(get_feature_vectors(song_file_path, audio_genre=genre))
+    test = pd.DataFrame.from_records(test_data, columns=headers)
+    X_test = test.loc[:, features]
+
+    prediction = model.predict(X_test)
+    counts = pd.DataFrame(prediction).value_counts()
+    prediction_mode = pd.DataFrame(prediction).mode()
+    if not testing:
+        return f"{num_to_genre[prediction_mode.iloc[0][0]]}"
+    else:
+        return f"({genre}) {os.path.basename(song_file_path)}", (num_to_genre[prediction_mode.iloc[0][0]], counts)
+
+
+if __name__ == "__main__":
+    # Figure out what to import the csv file
+    df = pd.read_csv('song_features_4genre_v2.csv', index_col='file_name')
+
+    # the genre in the dataset is a string and the ai cant run on a string, so you change it to a number here
+    df['genre'] = df.genre.map({'rock': 0, 'hiphop': 1, 'pop': 2, 'country': 3})
+
+    # create x and y
+    # x is a separate file with just the train data
+    # y is a separate file with the corresponding target values
+    X = df.loc[:, features]
+    y = df['genre']
+
+    # split the dataset into testing and training data using train_test_split
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+
     # create an instance of the model
     clf = DecisionTreeClassifier(max_depth=50, class_weight="balanced")
 
@@ -84,14 +99,9 @@ if __name__ == "__main__":
     pop_test_files = load_files("./test_songs/pop/")
     country_test_files = load_files("./test_songs/country/")
 
-    num_to_genre = {
-        0: 'rock',
-        1: 'hiphop',
-        2: 'pop',
-        3: 'country'
-    }
     results = {}
     # calculate predictions on test songs
+
     for path in rock_test_files:
         k, v = predict_song_genre(path, "rock")
         results[k] = v
@@ -107,7 +117,7 @@ if __name__ == "__main__":
     for path in country_test_files:
         k, v = predict_song_genre(path, "country")
         results[k] = v
-
+    
     scores = {
         "rock": 0,
         "hiphop": 0,
@@ -141,7 +151,6 @@ if __name__ == "__main__":
     print(clf.get_depth())
     print(clf.feature_importances_)
     print(sum(clf.feature_importances_))
-    '''
-    joblib.dump(clf, 'genre_identifier_model.pkl')
-    clf_load = joblib.load('genre_identifier_model.pkl')
-    print(clf_load.score(X_test, y_test))'''
+
+    #joblib.dump(clf, 'TEST1.pkl')
+    #clf_load = joblib.load('TEST1.pkl')
